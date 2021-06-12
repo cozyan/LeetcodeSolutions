71188f6834d893afd6023881cda26ce0f7e19dd4
cozyan <37967643+cozyan@users.noreply.github.com>
2021-06-12 18:14:28 +0200

    Update and rename 937. Reorder Data in Log Files to 937. Reorder Data in Log Files.py

--- 937. Reorder Data in Log Files.py
+++ 937. Reorder Data in Log Files.py
@@ -1,32 +1,32 @@
-937. Reorder Data in Log Files
+# 937. Reorder Data in Log Files
 
-## Description
-You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
+# ## Description
+# You are given an array of logs. Each log is a space-delimited string of words, where the first word is the identifier.
 
-There are two types of logs:
+# There are two types of logs:
 
-Letter-logs: All words (except the identifier) consist of lowercase English letters.
-Digit-logs: All words (except the identifier) consist of digits.
-Reorder these logs so that:
+# Letter-logs: All words (except the identifier) consist of lowercase English letters.
+# Digit-logs: All words (except the identifier) consist of digits.
+# Reorder these logs so that:
 
-The letter-logs come before all digit-logs.
-The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
-The digit-logs maintain their relative ordering.
-Return the final order of the logs.
+# The letter-logs come before all digit-logs.
+# The letter-logs are sorted lexicographically by their contents. If their contents are the same, then sort them lexicographically by their identifiers.
+# The digit-logs maintain their relative ordering.
+# Return the final order of the logs.
 
  
 
-Example 1:
+# Example 1:
 
-Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
-Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
-Explanation:
-The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
-The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
-Example 2:
+# Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+# Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+# Explanation:
+# The letter-log contents are all different, so their ordering is "art can", "art zero", "own kit dig".
+# The digit-logs have a relative order of "dig1 8 1 5 1", "dig2 3 6".
+# Example 2:
 
-Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
-Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
+# Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
+# Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
 
 
 ## Solution
@@ -68,12 +68,16 @@ class Solution:
         # Calling
         let.sort(key=functools.cmp_to_key(compare))
         return let+dig
-        
+"""
 Time Complexity: O(mnlog(n))
 Explaination: 
 Python list sort() function uses Timsort algorithm which has time complexity of O(nlogn). 
 In our question, n means the total number of letter logs. We then consider the compare() function within the letter log,
 this will take up m time for each comparison. So in total it's mnlog(n).
+
 Space Complexity: O(mlog(n))
 Explaination:
-
+Python list sort space complexity is O(log(n)), since we compare each letter log, then it's O(mnlog(n)).
+Moreover, for each comparision, we need to temporarily store each letter log which also takes O(m) space.
+Intotal, it's O(m+mlog(n)) => O(mlog(n)) space.
+"""
